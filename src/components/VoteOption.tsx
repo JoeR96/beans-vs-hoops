4854246244b8f74c719330a15ea7a8fb37dbab1e
Joe Richardson <joeyrichardson96@gmail.com>
2023-07-16 13:30:24 +0100

    fix multi vote

--- src/components/VoteOption.tsx
+++ src/components/VoteOption.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '@mui/material';
 
 interface VoteOptionProps {
@@ -8,16 +8,17 @@ interface VoteOptionProps {
 }
 
 const VoteOption: React.FC<VoteOptionProps> = ({ imageUrl, altText, onVote }) => {
+  const [isVoting, setIsVoting] = useState(false);
+
   const handleVote = async () => {
+    setIsVoting(true);
     await onVote();
   }
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <img src={imageUrl} alt={altText} style={{ width: '50%', height: 'auto' }} />
-      <Button variant="contained" onClick={handleVote}> 
-        Vote
-      </Button>
+      {!isVoting && <Button variant="contained" onClick={handleVote}>Vote</Button>}
     </div>
   );
 };
